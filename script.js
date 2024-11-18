16a3c9e30dfa13ea172d25def9fe9f7d17cf1f0c
JITENDER <138762808+jeetsingh-621@users.noreply.github.com>
2024-11-18 11:30:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const btn = document.getElementById("enterBtn");
+const title = document.getElementById("status");
+
+btn.addEventListener("click",()=>{
+	title.innerHTML=`<h1 id='status'>Entered Metaverse </h1>`
+})
